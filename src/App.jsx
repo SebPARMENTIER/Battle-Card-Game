88a8a9e5e15609bb46fc4f876be07f325bd8fe54
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2022-01-24 15:09:58 +0100

    Create 2 decks for 2 players and create draw function

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,194 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [deckId, setDeckId] = useState();
+  const [cards1, setCards1] = useState([]);
+  const [cards2, setCards2] = useState([]);
+  const [isPlaying1, setIsPlaying1] = useState(false);
+  const [isPlaying2, setIsPlaying2] = useState(false);
+
+  const getDeck = () => {
+    const fetchData = async () => {
+      const res = await fetch("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+      const data = await res.json();
+      setDeckId(data.deck_id);
+    }
+    fetchData();
+  };
+
+  console.log(deckId);
+
+  const [deckPlayer1, setDeckPlayer1] = useState([]);
+  const [deckPlayer2, setDeckPlayer2] = useState([]);
+
+  const drawCardsPlayer1 = () => {
+    const fetchDataCardsPlayer1 = async () => {
+      const res = await fetch(`http://deckofcardsapi.com/api/deck/${deckId}/draw/?count=26`);
+      const data = await res.json();
+      setDeckPlayer1(data.cards)
+      
+    }
+    fetchDataCardsPlayer1();
+    setIsPlaying1(true);
+  };
+
+  console.log("cards player 1", deckPlayer1);
+
+  const drawCardsPlayer2 = () => {
+    const fetchDataCardsPlayer2 = async () => {
+      const res = await fetch(`http://deckofcardsapi.com/api/deck/${deckId}/draw/?count=26`);
+      const data = await res.json();
+      setDeckPlayer2(data.cards)
+      
+    }
+    fetchDataCardsPlayer2();
+    setIsPlaying2(true);
+  };
+
+  console.log("cards player 2", deckPlayer2);
+
+  const pilePlayer1 = "pilePlayer1";
+  
+  const deckPlayer1Array = [];  
+
+  deckPlayer1.map((d) => {
+      deckPlayer1Array.push(d.code);
+    });
+
+  const pilePlayer1Data = () => {
+    
+    const fetchDataPilePlayer1 = async () => {
+      const res = await fetch(`http://deckofcardsapi.com/api/deck/${deckId}/pile/${pilePlayer1}/add/?cards=${deckPlayer1Array.join(',')}`);
+      const data = await res.json();
+      setCards1(data.piles);
+      //console.log("cards", cards);
+    }
+    fetchDataPilePlayer1();
+  };
+  
+  const [list1, setList1] = useState();
+
+  const listPlayer1 = () => {
+    const fetchDataList1 = async () => {
+      const res = await fetch(`http://deckofcardsapi.com/api/deck/${deckId}/pile/${pilePlayer1}/list`);
+      const data = await res.json();
+      setList1(data);
+    }
+    fetchDataList1();
+  };
+
+  const pilePlayer2 = "pilePlayer2";
+  
+  const deckPlayer2Array = [];  
+
+  deckPlayer2.map((d) => {
+      deckPlayer2Array.push(d.code);
+    });
+
+  const pilePlayer2Data = () => {
+    
+    const fetchDataPilePlayer2 = async () => {
+      const res = await fetch(`http://deckofcardsapi.com/api/deck/${deckId}/pile/${pilePlayer2}/add/?cards=${deckPlayer2Array.join(',')}`);
+      const data = await res.json();
+      setCards2(data.piles);
+      //console.log("cards", cards);
+    }
+    fetchDataPilePlayer2();
+  };
+  
+  const [list2, setList2] = useState();
+
+  const listPlayer2 = () => {
+    const fetchDataList2 = async () => {
+      const res = await fetch(`http://deckofcardsapi.com/api/deck/${deckId}/pile/${pilePlayer2}/list`);
+      const data = await res.json();
+      setList2(data);
+    }
+    fetchDataList2();
+  };
+
+  console.log("liste 1", list1);
+
+  console.log("deck 1", deckPlayer1Array);
+
+  console.log("pile 1", cards1);
+
+  console.log("liste 2", list2);
+
+  console.log("deck 2", deckPlayer2Array);
+
+  console.log("pile 2", cards2);
+
+
+  const [cardPlayer1, setCardPlayer1] = useState([]);
+
+  const drawCardPlayer1 = () => {
+    const fetchDataDrawCardPlayer1 = async () => {
+      const res = await fetch(`http://deckofcardsapi.com/api/deck/${deckId}/pile/${pilePlayer1}/draw/?count=1`);
+      const data = await res.json();
+      setCardPlayer1(data.cards);
+    }
+    fetchDataDrawCardPlayer1();
+  };
+
+  const [cardPlayer2, setCardPlayer2] = useState([]);
+
+  const drawCardPlayer2 = () => {
+    const fetchDataDrawCardPlayer2 = async () => {
+      const res = await fetch(`http://deckofcardsapi.com/api/deck/${deckId}/pile/${pilePlayer2}/draw/?count=1`);
+      const data = await res.json();
+      setCardPlayer2(data.cards);
+    }
+    fetchDataDrawCardPlayer2();
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <button onClick={getDeck} className="App-button">Jouer</button>
+      <button onClick={drawCardsPlayer1} className="App-button">Play1</button>
+      <button onClick={drawCardsPlayer2} className="App-button">Play2</button>
+      <button onClick={pilePlayer1Data} className="App-button">Pile1</button>
+      <button onClick={listPlayer1} className="App-button">Liste1</button>
+      <button onClick={pilePlayer2Data} className="App-button">Pile2</button>
+      <button onClick={listPlayer2} className="App-button">Liste2</button>
+      <div className="App-area">
+        <div className="App-area-player1">
+          <div className="App-area-player1-deck">
+            <button
+              className="App-area-player1-deck-draw"
+              onClick={drawCardPlayer1}></button>
+          </div>
+          <div className="App-area-player1-play">
+            {cardPlayer1.map((c) => (            
+              <img
+                key={c.code}
+                src={c.image}
+                alt={c.code}
+                className="App-area-player1-play-img"
+              />
+            ))}
+          </div>
+        </div>
+        <div className="App-area-player2">
+          <div className="App-area-player2-play">
+            {cardPlayer2.map((c) => (
+              <img
+                key={c.code}
+                src={c.image}
+                alt={c.code}
+                className="App-area-player2-play-img"
+              />
+            ))}
+            </div>
+          <div className="App-area-player2-deck">
+            <button
+              className="App-area-player2-deck-draw"
+              onClick={drawCardPlayer2}
+            ></button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
