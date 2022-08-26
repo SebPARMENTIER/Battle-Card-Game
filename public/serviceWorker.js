f936c0aff9cda76039f8bcaedc937db8820407ec
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2022-08-26 17:40:11 +0200

    Add offline.html

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,5 +1,5 @@
 const BASE = location.protocol + "//" + location.host;
-const PREFIX = "V6";
+const PREFIX = "V1";
 const CACHED_FILES = [
   // `${BASE}/src/main.jsx`,
   // `${BASE}/src/App.jsx`,
@@ -86,7 +86,7 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     (async () => {
       const cache = await caches.open(PREFIX);
-      await cache.addAll([...CACHED_FILES]);
+      await cache.addAll([...CACHED_FILES, "/offline.html"]);
     })()
   );
   console.log(`${PREFIX} Install`);
